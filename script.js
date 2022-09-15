525d8f5bc94e2246d5a57970bcfcffb5a641a1fa
mmi-mamun <muzahidulislam735@gmail.com>
2022-09-15 22:58:17 +0600

    Histories are set as column for large devices and one history in one row for mobile devices

--- script.js
+++ script.js
@@ -68,7 +68,8 @@ const gameOver = () => {
   // the current time is the finish time
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
-  const timeTaken = (finishTime - startTime) / 1000;
+  const timeTakenFloat = (finishTime - startTime) / 1000;
+  const timeTaken = parseInt(timeTakenFloat);
 
   // show result modal
   resultModal.innerHTML = "";
