af87fb71d15b2d3b60740b2a588f5fe51c272940
mmi-mamun <muzahidulislam735@gmail.com>
2022-09-15 10:13:34 +0600

    error counter is working

--- script.js
+++ script.js
@@ -45,6 +45,7 @@ const typeController = (e) => {
   if (newLetterCorrect) {
     display.innerHTML += `<span class="green">${newLetter === " " ? "▪" : newLetter}</span>`;
   } else {
+    errorCount ++;  //modified: add this line for error count
     display.innerHTML += `<span class="red">${newLetter === " " ? "▪" : newLetter}</span>`;
   }
 
