802b64c4ad1d0b2d36216b0fb214637c18a20947
mmi-mamun <muzahidulislam735@gmail.com>
2022-09-15 11:20:14 +0600

    displayHistory() function is modified for showing last history is in top position

--- history.js
+++ history.js
@@ -24,20 +24,17 @@ function addHistory(questionText, timeTaken, errorCount) {
 function displayHistory() {
   histories.innerHTML = "";
   const previousTests = JSON.parse(localStorage.getItem("testHistory")) || [];
+  console.log(previousTests);
+  previousTests.slice().reverse().forEach((test) => {
+      const newRow = document.createElement("div");
+      newRow.classList.add("card");
 
-  previousTests.forEach((test) => {
-    const newRow = document.createElement("div");
-    newRow.classList.add("card");
+      newRow.innerHTML = `
+        <h3>${test.questionText}</h3>
+        <p>You took: <span class="bold">${test.timeTaken}</span> seconds</p>
+        <p>You made <span class="bold red">${test.errorCount}</span> mistakes</p>
+      `;
 
-    newRow.innerHTML = `
-  <h3>${test.questionText}</h3>
-
-  <p  style="margin-bottom: 10px">You took: <span class="bold">${test.timeTaken}</span> seconds</p>
-
-    <p  style="margin-bottom: 10px">You made <span class="bold red">${test.errorCount}</span> mistakes</p>
-
-  `;
-
-    histories.appendChild(newRow);
-  });
+      histories.appendChild(newRow);
+    });
 }
